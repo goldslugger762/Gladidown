f63eb3366c9c4524feffc77fa028c5308216f8b2
kasya <kasya@DESKTOP-5IG6AAM>
2023-12-09 22:58:49 +0300

    Добавьте файлы проекта.

---
+++ etozhestkapec/Form1.Designer.cs
@@ -0,0 +1,131 @@
+﻿namespace etozhestkapec
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.txtammo = new System.Windows.Forms.Label();
+            this.txtscore = new System.Windows.Forms.Label();
+            this.txthealth = new System.Windows.Forms.Label();
+            this.healthbar = new System.Windows.Forms.ProgressBar();
+            this.gametimer = new System.Windows.Forms.Timer(this.components);
+            this.player = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.player)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // txtammo
+            // 
+            this.txtammo.AutoSize = true;
+            this.txtammo.Font = new System.Drawing.Font("Georgia", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.txtammo.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.txtammo.Location = new System.Drawing.Point(12, 9);
+            this.txtammo.Name = "txtammo";
+            this.txtammo.Size = new System.Drawing.Size(100, 25);
+            this.txtammo.TabIndex = 0;
+            this.txtammo.Text = "Ammo: 0";
+            this.txtammo.Click += new System.EventHandler(this.txtammo_Click);
+            // 
+            // txtscore
+            // 
+            this.txtscore.AutoSize = true;
+            this.txtscore.Font = new System.Drawing.Font("Georgia", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.txtscore.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.txtscore.Location = new System.Drawing.Point(762, 9);
+            this.txtscore.Name = "txtscore";
+            this.txtscore.Size = new System.Drawing.Size(110, 25);
+            this.txtscore.TabIndex = 1;
+            this.txtscore.Text = "Corpses: 0";
+            // 
+            // txthealth
+            // 
+            this.txthealth.AutoSize = true;
+            this.txthealth.Font = new System.Drawing.Font("Georgia", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.txthealth.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.txthealth.Location = new System.Drawing.Point(270, 14);
+            this.txthealth.Name = "txthealth";
+            this.txthealth.Size = new System.Drawing.Size(80, 18);
+            this.txthealth.TabIndex = 2;
+            this.txthealth.Text = "Still alive?";
+            // 
+            // healthbar
+            // 
+            this.healthbar.Location = new System.Drawing.Point(356, 14);
+            this.healthbar.Name = "healthbar";
+            this.healthbar.Size = new System.Drawing.Size(197, 23);
+            this.healthbar.TabIndex = 3;
+            this.healthbar.Value = 100;
+            this.healthbar.Click += new System.EventHandler(this.healthbar_Click);
+            // 
+            // gametimer
+            // 
+            this.gametimer.Enabled = true;
+            this.gametimer.Interval = 20;
+            this.gametimer.Tick += new System.EventHandler(this.MainTimerEvent);
+            // 
+            // player
+            // 
+            this.player.Image = global::etozhestkapec.Properties.Resources.up;
+            this.player.Location = new System.Drawing.Point(370, 582);
+            this.player.Name = "player";
+            this.player.Size = new System.Drawing.Size(57, 62);
+            this.player.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.player.TabIndex = 4;
+            this.player.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Wheat;
+            this.ClientSize = new System.Drawing.Size(884, 681);
+            this.Controls.Add(this.player);
+            this.Controls.Add(this.healthbar);
+            this.Controls.Add(this.txthealth);
+            this.Controls.Add(this.txtscore);
+            this.Controls.Add(this.txtammo);
+            this.Name = "Form1";
+            this.Text = "Gladidown\'";
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.KeyDownIs);
+            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.KeyUpIs);
+            ((System.ComponentModel.ISupportInitialize)(this.player)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label txtammo;
+        private System.Windows.Forms.Label txtscore;
+        private System.Windows.Forms.Label txthealth;
+        private System.Windows.Forms.ProgressBar healthbar;
+        private System.Windows.Forms.PictureBox player;
+        private System.Windows.Forms.Timer gametimer;
+    }
+}
+
---
+++ etozhestkapec/Form1.cs
@@ -0,0 +1,157 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace etozhestkapec
+{
+    public partial class Form1 : Form
+    {
+
+
+        bool goLeft, goRight, goUp, goDown, gameOver;
+        string facing = "up";
+        int playerHealth = 100;
+        int speed = 10;
+        int ammo = 10;
+        int zedSpeed = 3;
+        Random randNum = new Random();
+        int score;
+
+        List<PictureBox> zedList = new List<PictureBox>();
+
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void txtammo_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void healthbar_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void MainTimerEvent(object sender, EventArgs e)
+        {
+            if (playerHealth > 1)
+            {
+                healthbar.Value = playerHealth; ;
+            }
+            else
+            {
+                gameOver = true;
+            }
+            txtammo.Text = "Ammo:" + ammo;
+            txtscore.Text = "Corpses:" + score;
+
+            if (goLeft == true && player.Left > 0)
+            {
+                player.Left -= speed;
+            }
+
+            if (goRight == true && player.Left + player.Width < this.ClientSize.Width)
+            {
+                player.Left += speed;
+            }
+
+            if (goUp == true && player.Top > 43)
+            {
+                player.Top -= speed;
+            }
+
+            if (goDown == true && player.Top + player.Height < this.ClientSize.Height)
+            {
+                player.Top += speed;
+            }
+        }
+
+        private void KeyDownIs(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Left)
+            {
+                goLeft = true;
+                facing = "left";
+                player.Image = Properties.Resources.left;
+            }
+
+            if (e.KeyCode == Keys.Right)
+            {
+                goRight = true;
+                facing = "right";
+                player.Image = Properties.Resources.right;
+            }
+
+            if (e.KeyCode == Keys.Up)
+            {
+                goUp = true;
+                facing = "up";
+                player.Image = Properties.Resources.up;
+            }
+
+            if (e.KeyCode == Keys.Down)
+            {
+                goDown = true;
+                facing = "down";
+                player.Image = Properties.Resources.down;
+            }
+        }
+
+        private void KeyUpIs(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Left)
+            {
+                goLeft = false;
+            }
+
+            if (e.KeyCode == Keys.Right)
+            {
+                goRight = false;
+            }
+
+            if (e.KeyCode == Keys.Up)
+            {
+                goUp = false;
+            }
+
+            if (e.KeyCode == Keys.Down)
+            {
+                goDown = false;
+            }
+
+            if (e.KeyCode == Keys.Space)
+            {
+                ShootBullet(facing);
+            }
+        }
+
+
+        private void ShootBullet(string direction)
+        {
+            Bullet shootBullet = new Bullet();
+            shootBullet.direction = direction;
+            shootBullet.bulletLeft = player.Left + (player.Width / 2);
+            shootBullet.bulletTop = player.Top + (player.Height / 2);
+            shootBullet.MakeBullet(this);
+        }
+
+        private void MakeZeds()
+        {
+
+        }
+
+        private void RestartGame()
+        {
+
+        }
+    }
+}
